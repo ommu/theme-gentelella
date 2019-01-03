48aed9337dd1afc21a1bcc1e76739bc7a05707fb
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-01-03 16:14:56 +0700

    commit

--- components/Menu.php
+++ components/Menu.php
@@ -1,14 +1,17 @@
 <?php
-namespace themes\gentelella\components;
-
 /**
  * Menu class
- *
  * Klas turunan dari klas menu gentelella dengan ditambahkan penangan javascript void pada menu yg mengandung #
  *
- * @author    Agus Susilo<smartgdi@gmail.com>
- * @copyright Copyright (c) 2016 ECC UGM
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2018 OMMU (www.ommu.co)
+ * @created date 2 January 2018, 23:08 WIB
+ * @link https://github.com/ommu/ommu
+ * 
  */
+
+namespace themes\gentelella\components;
 
 use Yii;
 use rmrevin\yii\fontawesome\component\Icon;
