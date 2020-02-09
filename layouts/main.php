68d05259bde8f0010a26e06c32f88060918657c1
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2020-02-10 06:28:29 +0700

    add overflow in custom css, and add cards conditin in layout

--- layouts/main.php
+++ layouts/main.php
@@ -11,27 +11,32 @@
 
 <div class="row">
 	<div class="col-md-12 col-sm-12 col-xs-12">
-		<?php echo \app\components\widgets\Alert::widget(['closeButton'=>false]); ?>
+		<?php echo \app\components\widgets\Alert::widget(['closeButton'=>false]);
+		
+		if($this->cards == false)
+			echo $content;
 
-		<div class="x_panel">
-			<div class="x_title">
-				<?php echo !empty($this->params['menu']['content']) ? \app\components\widgets\MenuContent::widget(['items' => $this->params['menu']['content']]) : '';?>
-				<ul class="nav navbar-right panel_toolbox">
-					<li><a href="#" title="<?php echo Yii::t('app', 'Toggle');?>" class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-					<?php if(!empty($this->params['menu']['option'])):?>
-					<li class="dropdown">
-						<a href="#" title="<?php echo Yii::t('app', 'Options');?>" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-						<?php echo \app\components\widgets\MenuOption::widget(['items' => $this->params['menu']['option']]);?>
-					</li>
-					<?php endif;?>
-					<li><a href="#" title="<?php echo Yii::t('app', 'Close');?>" class="close-link"><i class="fa fa-close"></i></a></li>
-				</ul>
-				<div class="clearfix"></div>
+		else {?>
+			<div class="x_panel">
+				<div class="x_title">
+					<?php echo !empty($this->params['menu']['content']) ? \app\components\widgets\MenuContent::widget(['items' => $this->params['menu']['content']]) : '';?>
+					<ul class="nav navbar-right panel_toolbox">
+						<li><a href="#" title="<?php echo Yii::t('app', 'Toggle');?>" class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+						<?php if(!empty($this->params['menu']['option'])):?>
+						<li class="dropdown">
+							<a href="#" title="<?php echo Yii::t('app', 'Options');?>" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+							<?php echo \app\components\widgets\MenuOption::widget(['items' => $this->params['menu']['option']]);?>
+						</li>
+						<?php endif;?>
+						<li><a href="#" title="<?php echo Yii::t('app', 'Close');?>" class="close-link"><i class="fa fa-close"></i></a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div class="x_content">
+					<?php echo $content; ?>
+				</div>
 			</div>
-			<div class="x_content">
-				<?php echo $content; ?>
-			</div>
-		</div>
+		<?php }?>
 	</div>
 </div>
 
--- layouts/main_submenu.php
+++ layouts/main_submenu.php
@@ -31,28 +31,34 @@ foreach ($menus as $i => $menu) {
 
 <div class="row">
 	<div class="col-sm-9 col-xs-12">
-		<?php echo \app\components\widgets\Alert::widget(['closeButton'=>false]); ?>
+		<?php echo \app\components\widgets\Alert::widget(['closeButton'=>false]);
+		
+		if($this->cards == false)
+			echo $content;
 
-		<div class="x_panel">
-			<div class="x_title">
-				<?php echo !empty($this->params['menu']['content']) ? MenuContent::widget(['items' => $this->params['menu']['content']]) : '';?>
-				<ul class="nav navbar-right panel_toolbox">
-					<li><a href="#" title="<?php echo Yii::t('app', 'Toggle');?>" class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-					<?php if(!empty($this->params['menu']['option'])):?>
-					<li class="dropdown">
-						<a href="#" title="<?php echo Yii::t('app', 'Options');?>" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-						<?php echo MenuOption::widget(['items' => $this->params['menu']['option']]);?>
-					</li>
-					<?php endif;?>
-					<li><a href="#" title="<?php echo Yii::t('app', 'Close');?>" class="close-link"><i class="fa fa-close"></i></a></li>
-				</ul>
-				<div class="clearfix"></div>
+		else {?>
+			<div class="x_panel">
+				<div class="x_title">
+					<?php echo !empty($this->params['menu']['content']) ? MenuContent::widget(['items' => $this->params['menu']['content']]) : '';?>
+					<ul class="nav navbar-right panel_toolbox">
+						<li><a href="#" title="<?php echo Yii::t('app', 'Toggle');?>" class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+						<?php if(!empty($this->params['menu']['option'])):?>
+						<li class="dropdown">
+							<a href="#" title="<?php echo Yii::t('app', 'Options');?>" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+							<?php echo MenuOption::widget(['items' => $this->params['menu']['option']]);?>
+						</li>
+						<?php endif;?>
+						<li><a href="#" title="<?php echo Yii::t('app', 'Close');?>" class="close-link"><i class="fa fa-close"></i></a></li>
+					</ul>
+					<div class="clearfix"></div>
+				</div>
+				<div class="x_content">
+					<?php echo $content; ?>
+				</div>
 			</div>
-			<div class="x_content">
-				<?php echo $content; ?>
-			</div>
-		</div>
+		<?php }?>
 	</div>
+
 	<div class="col-sm-3 col-xs-12">
 		<div class="list-group">
 			<?php foreach ($menus as $menu) {
