56b40b53807759473c7ab0d57e0340ce35feae71
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2022-09-02 07:20:37 +0700

    add flex in bootstrap

--- components/views/menu_footer.php
+++ components/views/menu_footer.php
@@ -1,10 +1,13 @@
 
 <?php /* footer content */?>
-<footer>
-	<div class="pull-right">
+<footer class="d-flex flex-md-row flex-column">
+	<div class="order-md-1 order-0 text-md-right text-center">
 		Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com" rel="nofollow" target="_blank">Colorlib</a> |
 		Extension for Yii framework 2 by <a href="http://yiister.ru" rel="nofollow" target="_blank">Yiister</a>
 	</div>
-	<div class="clearfix"></div>
+	<div class="order-md-0 order-1 text-md-left text-center">
+        <form>
+        123
+	</div>
 </footer>
 <?php /* end.footer content */?>
