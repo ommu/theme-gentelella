39df10c134d382ee3d7a7a93132c993dcf8a7b98
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2022-09-02 09:04:52 +0700

    add selectLanguage in footer

--- components/MenuFooter.php
+++ components/MenuFooter.php
@@ -1,9 +1,21 @@
 <?php
 namespace themes\gentelella\components;
 
+use app\models\CoreLanguages;
+use yii\helpers\ArrayHelper;
+
 class MenuFooter extends \yii\base\Widget
 {
 	public function run() {
-		return $this->render('menu_footer');
+        
+        $coreLang = CoreLanguages::find()
+            ->select(['code', 'name'])
+            ->where(['actived'=> 1])
+            ->all();
+        $language = ArrayHelper::map($coreLang, 'code', 'name');
+
+		return $this->render('menu_footer', [
+            'language' => $language,
+        ]);
 	}
 }
--- components/views/menu_footer.php
+++ components/views/menu_footer.php
@@ -1,13 +1,39 @@
+<?php 
+use yii\helpers\Html;
+use yii\helpers\Url;
 
-<?php /* footer content */?>
+$js = <<<JS
+    $(document).on('change', 'form.selectLang', function() {
+        var url = $(this).attr('action');
+		var options = {
+			type: 'POST',
+			data: $(this).serialize(),
+			success: function(response, textStatus, jqXHR) {
+				location.href = url;
+			},
+			complete: function(jqXHR, textStatus) {
+				var redirect = jqXHR.getResponseHeader('X-Redirect');
+				if(redirect != null) {
+					location.href = redirect;
+                }
+			}
+		}
+        $.ajax(url, options);
+    });
+JS;
+$this->registerJs($js, $this::POS_END);
+
+/* footer content */?>
 <footer class="d-flex flex-md-row flex-column">
-	<div class="order-md-1 order-0 text-md-right text-center">
+	<div class="flex-fill order-md-1 order-0 text-md-right text-center">
 		Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com" rel="nofollow" target="_blank">Colorlib</a> |
 		Extension for Yii framework 2 by <a href="http://yiister.ru" rel="nofollow" target="_blank">Yiister</a>
 	</div>
-	<div class="order-md-0 order-1 text-md-left text-center">
-        <form>
-        123
+	<div class="order-md-0 order-1 text-md-left text-center mt-md-0 mt-5">
+        <?php 
+        echo Html::beginForm(Yii::$app->request->url, 'POST', ['class' => 'selectLang']);
+        echo Html::dropDownList('lang', Yii::$app->language, $language, ['class' => 'form-control']);
+        echo Html::endForm(); ?>
 	</div>
 </footer>
 <?php /* end.footer content */?>
