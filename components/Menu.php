81978c9b4b364814b8d19447b9b97ccab65d8c6d
Putra Sudaryanto <putra@ommu.id>
2025-08-04 00:01:57 +0700

    modify author phonenumber

--- components/Menu.php
+++ components/Menu.php
@@ -4,7 +4,7 @@
  * Klas turunan dari klas menu gentelella dengan ditambahkan penangan javascript void pada menu yg mengandung #
  *
  * @author Putra Sudaryanto <putra@ommu.id>
- * @contact (+62)856-299-4114
+ * @contact (+62)811-2540-432
  * @copyright Copyright (c) 2018 OMMU (www.ommu.id)
  * @created date 2 January 2018, 23:08 WIB
  * @link https://github.com/ommu/theme-gentelella
